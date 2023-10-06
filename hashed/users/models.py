4d454cd3dde9e1f8f407ca17dcaf34e879ea2967
RoopamTaneja <roopamtaneja999@gmail.com>
2023-10-06 19:04:53 +0530

    models created

--- hashed/users/models.py
+++ hashed/users/models.py
@@ -1,20 +1,29 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.hashers import make_password, check_password
+import uuid
+
 
 class user(models.Model):
-    #usertoken
-    username = models.CharField(max_length=50, default="")
-    f_name = models.CharField(max_length=50, default="")
-    l_name = models.CharField(max_length=50, default="")
-    email = models.EmailField(max_length=30, default="")
-    #hashed pass
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=50, blank=False, unique=True)
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    email = models.EmailField(blank=False, unique=True)
+    date_joined = models.DateTimeField(default=timezone.now, editable=False)
+    hashed_password = models.CharField(blank=False)
+    hashed_pin = models.CharField(blank=False, editable=False)
+
     def __str__(self):
         return self.username
-    
+
+
 class credential(models.Model):
-    #usertoken
-    title = models.CharField(max_length=50, default="")
-    website = models.URLField(max_length=100)
-    #hashed pass
-    #strength
+    user = models.OneToOneField(user, on_delete=models.CASCADE, primary_key=True)
+    title = models.CharField(max_length=50, blank=True)
+    website = models.URLField(blank=False)
+    hash_pwd = models.CharField(blank=False)
+    strength = models.IntegerField(blank=False)
+
     def __str__(self):
         return self.title
