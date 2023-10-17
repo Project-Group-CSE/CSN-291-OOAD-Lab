66160ecff324746907ac0e8a8b8efcd8c227b69f
timpg18 <paritgupta8@gmail.com>
2023-10-17 14:26:29 +0530

    Revert "Merge branch 'main' into parit"
    This reverts commit 607fcc9c993e390d0213228e0517af65405dbb2a, reversing
    changes made to 702f6db85b79c59d03baabbe717faed1b4d13004.

--- hashed/users/admin.py
+++ hashed/users/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from users.models import user, credential
+from users.models import user
 # Register your models here.
 
 admin.site.register(user)
-admin.site.register(credential)
--- random_good_pass.py
+++ random_good_pass.py
@@ -1,6 +1,6 @@
 import requests
-import hashlib
-import re
+
+
 def get_pass(num):
     api = "https://random-word-api.herokuapp.com/word"
     parameters = {"number": num}
@@ -15,44 +15,6 @@ def get_pass(num):
         s += result[num - 1]
         return s
 
-def check_password_pwned(password):
-    # Hash the password using SHA-1 hash algorithm
-    sha1_password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-    prefix, suffix = sha1_password[:5], sha1_password[5:]
-    api_url = f"https://api.pwnedpasswords.com/range/{prefix}"
-    response = requests.get(api_url)
-    if response.status_code == 200:
-        
-        for line in response.text.splitlines():
-            if line.startswith(suffix):
-                count = int(line.split(":")[1])
-                return f"The password has been found {count} times and is compromised."
-        return "The password has not been found in any known breaches."
-    elif response.status_code == 404:
-        return "Invalid API endpoint or no data found."
-    else:
-        return "Error occurred while checking the password."
 
-
-
-def password_strength(password):
-    
-    very_weak_pattern = r'^.{1,5}$' 
-    weak_pattern = r'^(?=.*[a-zA-Z])(?=.*\d).{6,}$'
-    medium_pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$'
-    strong_pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=]).{8,}$'  
-
-   
-    if re.match(strong_pattern, password):
-        return "Strong"
-    elif re.match(medium_pattern, password):
-        return "Medium"
-    elif re.match(weak_pattern, password):
-        return "Weak"
-    elif re.match(very_weak_pattern, password):
-        return "Very Weak"
-    else:
-        return "Invalid"
-
-
-
+ans = get_pass(7)
+print(ans)
